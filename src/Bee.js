8b9a0b74780ca3f00ed1e19eb3fbc092b1c27f0b
miriam30 <miriam.alvarez.p96@gmail.com>
2018-01-22 15:24:55 -0600

    finished grub and bee

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,8 @@
-class Bee {
-  // TODO..
+class Bee extends Grub {
+  constructor(food, age, color, job){
+    super(food)
+    this.age = 5;
+    this.color = "yellow";
+    this.job = "Keep on growing";
+  }
 };
--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,14 @@
+
 class Grub {
+  constructor(age, color,food){
+    this.age = 0;
+    this.color = "pink";
+    this.food = "jelly";
+  }
+
+
+//let grub = new Grub();
+eat(){
+  return "Mmmmmmmmm " + this.food;
+};
 };
